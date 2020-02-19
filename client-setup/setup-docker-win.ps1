380c006a5932b7ff1e9644490a4ba04827c9d2b2
Tsvetan Ivanov <tsvetan.ivanov@accedia.com>
2020-02-19 16:21:39 +0200

    Add client setup scripts

---
+++ client-setup/setup-docker-win.ps1
@@ -0,0 +1,26 @@
+param(
+    [Parameter(Mandatory)]
+    [String]$dockerHost
+)
+
+#  check if docker cli is insalled
+if ((Get-Command "docker.exe" -ErrorAction SilentlyContinue) -eq $null) 
+{
+   Write-Host "Downloading docker cli and docker-compose cli"
+   mkdir "$HOME\docker-client"
+   cd "$HOME\docker-client"
+   wget https://github.com/Jessie365/docker-workshop/raw/develop/bin/docker.exe -OutFile docker.exe
+   wget https://github.com/Jessie365/docker-workshop/raw/develop/bin/docker-compose.exe -OutFile docker-compose.exe
+   $envPath = [System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::User)
+   $envPath += "$HOME\docker-client"
+   [System.Environment]::SetEnvironmentVariable('PATH', $envPath, [System.EnvironmentVariableTarget]::User)
+}
+
+if (([System.Environment]::GetEnvironmentVariable('DOCKER_HOST', [System.EnvironmentVariableTarget]::User)) -eq $null)
+{
+    [System.Environment]::SetEnvironmentVariable('DOCKER_HOST', $dockerHost, [System.EnvironmentVariableTarget]::User)
+}
+else
+{
+    Write-Host "WARNING! DOCKER_HOST is already set!"
+}
